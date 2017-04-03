e8f9e3bbd5b8b03722a2c3bc6ddc6cfb054ee445
nikita shaposhnikov <topru333@hotmail.com>
2017-04-03 07:20:07 +0300

    Added some buttons on settings menu

--- src/PointsAndKeys.java
+++ src/PointsAndKeys.java
@@ -1,10 +1,30 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTable;
+
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.Color;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Toolkit;
+import java.awt.Font;
+import javax.swing.UIManager;
+
+import java.awt.Window.Type;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PointsAndKeys {
 
 	private JFrame frame;
+	private JTable table;
+	private JTextField text;
 
 	/**
 	 * Launch the application.
@@ -34,8 +54,74 @@ public class PointsAndKeys {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 300, 300);
+		frame.setType(Type.UTILITY);
+		frame.getContentPane().setBackground(new Color(240, 248, 255));
+		frame.setBackground(UIManager.getColor("FormattedTextField.inactiveForeground"));
+		frame.setForeground(Color.BLACK);
+		frame.setFont(new Font("Action Man Extended", Font.PLAIN, 12));
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(PointsAndKeys.class.getResource("/javax/swing/plaf/metal/icons/ocean/computer.gif")));
+		frame.setBounds(100, 100, 315, 300);
+		
+		table = new JTable();
+		table.setBorder(null);
+		table.setBackground(new Color(169, 169, 169));
+		
+		text = new JTextField();
+		text.setFont(new Font("Tahoma", Font.BOLD, 12));
+		text.setBackground(new Color(169, 169, 169));
+		text.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Add");
+		
+		DefaultTableModel model = (DefaultTableModel) table.getModel(); 
+		model.addColumn("PointKeys");
+		table.setEnabled(false);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				model.addRow(new Object[]{text.getText().toUpperCase()});
+				text.setText("");
+			}
+		});
+		
+		JButton btnNewButton_1 = new JButton("Delete");
+		
+		JButton btnNewButton_2 = new JButton("Clear");
+		
+		JButton btnNewButton_3 = new JButton("Save");
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(table, GroupLayout.PREFERRED_SIZE, 153, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(btnNewButton_3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnNewButton_2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(text))
+					.addContainerGap(22, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(text, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnNewButton)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnNewButton_1)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnNewButton_2)
+							.addPreferredGap(ComponentPlacement.RELATED, 109, Short.MAX_VALUE)
+							.addComponent(btnNewButton_3))
+						.addComponent(table, GroupLayout.DEFAULT_SIZE, 239, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		frame.getContentPane().setLayout(groupLayout);
 
 	}
-
 }
